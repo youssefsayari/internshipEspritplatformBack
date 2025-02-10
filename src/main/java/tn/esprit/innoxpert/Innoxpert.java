99b4a3e33b7d03a9c533638727ebe82eb8d5dfd0
amenk21 <amenallah.kthiri@esprit.tn>
2025-02-10 19:16:40 +0100

    SKELETON_BACK

---
+++ src/main/java/tn/esprit/innoxpert/Innoxpert.java
@@ -0,0 +1,13 @@
+package tn.esprit.innoxpert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Innoxpert {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Innoxpert.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/innoxpert/AmenallahKthiri4Sae6ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.innoxpert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmenallahKthiri4Sae6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
