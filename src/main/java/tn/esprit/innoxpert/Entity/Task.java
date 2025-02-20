da0fcfcac5ec287cb5e9a03ec4cd99c0498e2b75
amenk21 <amenallah.kthiri@esprit.tn>
2025-02-20 21:04:20 +0100

    Update Task.java

--- src/main/java/tn/esprit/innoxpert/Entity/Task.java
+++ src/main/java/tn/esprit/innoxpert/Entity/Task.java
@@ -1,5 +1,6 @@
 package tn.esprit.innoxpert.Entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
 import lombok.*;
@@ -26,6 +27,7 @@ public class Task {
     TypeStatus status;
 
     @ManyToOne
+    @JsonIgnore
     @NotNull(message = "Un étudiant doit être assigné à cette tâche")
     User student;
 }
